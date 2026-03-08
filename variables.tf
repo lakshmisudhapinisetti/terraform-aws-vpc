fbc58ec1e9b04cee9b41a44020d81855a498dfb5
Lakshmi Sudha Pinisetti <lakshmisudhapinisetti@ibm.com>
2026-03-08 18:15:13 +0530

    actions

--- variables.tf
+++ variables.tf
@@ -17,8 +17,7 @@ variable "igw_tags"{
     type = map
     default = {}
 }
-variable "public_subnet_cidrs"
-{
+variable "public_subnet_cidrs"{
     type = list
     default = ["10.0.1.0/24" , "10.0.2.0/24"]
 }
