7d393853c0d95cf6201c5b1e6c4a4561894b2659
Lakshmi Sudha Pinisetti <lakshmisudhapinisetti@ibm.com>
2026-03-10 18:26:59 +0530

    actions

--- main.tf
+++ main.tf
@@ -103,11 +103,11 @@ resource "aws_route_table" "database" {
 resource "aws_route" "public" {
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.main.id
+  gateway_id = aws_nat_gateway.main.id
 }
 
 resource "aws_eip" "nat" {
-  instance = aws_instance.nat.id
+  instance = aws_instance.main.id
   domain   = "vpc"
   tags = merge(
         local.common_tags,
