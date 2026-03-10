b580be6a587aa9b0f9906cafe4aa10384162b6e5
Lakshmi Sudha Pinisetti <lakshmisudhapinisetti@ibm.com>
2026-03-10 16:33:03 +0530

    actions

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ resource "aws_internet_gateway" "gw" {
 
   tags = local.igw_final_tags
 }
+
 resource "aws_subnet" "public" {
     count = length(var.public_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
@@ -25,5 +26,38 @@ resource "aws_subnet" "public" {
             Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
         },
         var.public_subnet_tags
+    )
+    }
+
+resource "aws_subnet" "private" {
+    count = length(var.private_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(
+        local.common_tags,
+        # roboshop-dev-private-us-east-1a
+        {
+            Name = "${var.project}-${var.environment}-private-${local.az_names[count.index]}"
+        },
+        var.private_subnet_tags
+    )
+    }
+
+    resource "aws_subnet" "database" {
+    count = length(var.database_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+
+
+  tags = merge(
+        local.common_tags,
+        # roboshop-dev-database-us-east-1a
+        {
+            Name = "${var.project}-${var.environment}-database-${local.az_names[count.index]}"
+        },
+        var.database_subnet_tags
     )
     }
