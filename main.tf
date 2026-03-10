dabe5249e1b248961e3445d71dd1d5edc5ef0a25
Lakshmi Sudha Pinisetti <lakshmisudhapinisetti@ibm.com>
2026-03-10 18:49:14 +0530

    actions

--- main.tf
+++ main.tf
@@ -137,7 +137,7 @@ resource "aws_nat_gateway" "main" {
   depends_on = [aws_internet_gateway.gw]
 }
 resource "aws_route" "private" {
-  route_table_id            = aws_route_table.private
+  route_table_id            = aws_route_table.private.id
   destination_cidr_block    = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.main.id
 }
