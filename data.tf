5460693fe621b69ba930e4110ccba5a4308854a7
Lakshmi Sudha Pinisetti <lakshmisudhapinisetti@ibm.com>
2026-03-11 17:19:37 +0530

    actions

--- data.tf
+++ data.tf
@@ -12,11 +12,4 @@ data "aws_route_table" "default" {
     name   = "association.main"
     values = ["true"]
   }
-data "aws_route_table" "default" {
-  vpc_id = data.aws_vpc.default.id
-  filter {
-    name   = "association.main"
-    values = ["true"]
-  }
-}
 }
